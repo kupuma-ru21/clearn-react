fdde750da7ce4e3231bb7e0c6ae63c0a9d8993c7
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-10 22:47:14 +0900

    feat: ensure Login add accessToken to localStorage on success

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -8,5 +8,5 @@ export const mockAuthentication = (): AuthenticationParams => ({
 })
 
 export const mockAccountModel = (): AccountModel => ({
-  accesToken: faker.random.uuid()
+  accessToken: faker.random.uuid()
 })
