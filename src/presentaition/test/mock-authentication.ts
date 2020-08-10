fa3d20bed2a27e093e8a0f29fa36a08619443651
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-10 21:13:58 +0900

    feat: prevent Login to call submit more than once

--- src/presentaition/test/mock-authentication.ts
+++ src/presentaition/test/mock-authentication.ts
@@ -5,8 +5,10 @@ import { mockAccountModel } from '@/domain/test/mock-account'
 export class AuthenticationSpy implements Authentication {
   account = mockAccountModel();
   params: AuthenticationParams;
+  callsCount = 0;
   async auth (params: AuthenticationParams): Promise<AccountModel> {
     this.params = params
+    this.callsCount++
     return Promise.resolve(this.account)
   }
 }
