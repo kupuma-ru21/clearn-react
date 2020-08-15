5ab566fd31669efbcbed90ef047aa5d247189e87
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-15 16:51:45 +0900

    test: ensure Login present error state if form is invalid

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
       },
       {
         test: /\.jsx?$/,
-        exclude: [/node_modules/],
+        exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
