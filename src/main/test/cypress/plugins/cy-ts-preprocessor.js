54021e1cc5311148c3d2340f33c8791d831106a6
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-16 01:03:45 +0900

    fix: resolve  cypress error

--- src/main/test/cypress/plugins/cy-ts-preprocessor.js
+++ src/main/test/cypress/plugins/cy-ts-preprocessor.js
@@ -1,8 +1,7 @@
-const wp = require('@cypress/webpack-preprocessor')
+const webpack = require('@cypress/webpack-preprocessor');
 
-module.exports = wp({
-  resolve: { extensions: ['.ts', '.js'] },
-  module: {
-    rules: [{ test: /\.ts$/, exclude: /node_modules/, loader: 'ts-loader' }]
-  }
-})
+const options = {
+  webpackOptions: require('../webpack.config.js'),
+};
+
+module.exports = webpack(options);
---
+++ src/main/test/cypress/webpack.config.js
@@ -0,0 +1,20 @@
+// https://fullstackhq.io/the-no-tears-cypress-setup/
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  node: { fs: 'empty', child_process: 'empty', readline: 'empty' },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
