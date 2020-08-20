640d4eaec6ca81b79f1285a3d34166defc3a3d34
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-20 23:22:48 +0900

    test: ensure SignUp present valid present InvalidCredentinalsError on 401

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,5 +1,15 @@
 import faker from 'faker'
 import * as FormHelper from '../support/form-helper'
+import * as Http from '../support/login-mocks'
+
+const simulateValidSubmit = (): void => {
+  cy.getByTestId('name').focus().type(faker.random.alphaNumeric(7))
+  cy.getByTestId('email').focus().type(faker.internet.email())
+  const password = faker.random.alphaNumeric(7)
+  cy.getByTestId('password').focus().type(password)
+  cy.getByTestId('passwordConfirmation').focus().type(password)
+  cy.getByTestId('submit').click()
+}
 
 describe('SignUp', () => {
   beforeEach(() => {
@@ -43,7 +53,7 @@ describe('SignUp', () => {
   })
 
   it('Should present valid state if form is valid', () => {
-    cy.getByTestId('name').focus().type(faker.name.findName())
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(7))
     FormHelper.testInputStatus('name')
 
     cy.getByTestId('email').focus().type(faker.internet.email())
@@ -58,5 +68,12 @@ describe('SignUp', () => {
 
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present InvalidCredentinalsError on 403', () => {
+    Http.mockEmailInUseError()
+    simulateValidSubmit()
+    FormHelper.testMainError('Esse e-mail ja esta em uso')
+    FormHelper.testUrl('/signup')
   })
 })
