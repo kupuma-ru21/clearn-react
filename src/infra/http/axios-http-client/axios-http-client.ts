1d95aec8c9f68469e621db28dbd81d7d4e4afe97
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-23 11:30:47 +0900

    feat: ensure AxiosHttpClient returns correct value on failure

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -14,13 +14,17 @@ export class AxiosHttpClient implements HttpPostClient, HttpGetClient {
       axiosResponse = await axios.post(params.url, params.body)
     } catch (error) {
       axiosResponse = error.response
-      return { statusCode: axiosResponse.status, body: axiosResponse.data }
     }
     return { statusCode: axiosResponse.status, body: axiosResponse.data }
   }
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
-    const axiosResponse = await axios.get(params.url)
+    let axiosResponse: AxiosResponse
+    try {
+      axiosResponse = await axios.get(params.url)
+    } catch (error) {
+      axiosResponse = error.response
+    }
     return { statusCode: axiosResponse.status, body: axiosResponse.data }
   }
 }
