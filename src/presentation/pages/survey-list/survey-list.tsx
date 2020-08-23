a4eb51f2660afb345cb1356f94617516dbc3d71a
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-23 17:28:04 +0900

    feat: add skelton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Styles from './survey-list-styles.scss';
+import React from 'react'
+import Styles from './survey-list-styles.scss'
 import {
   Footer,
   SurveyListHeader,
   Icon,
-  IconName,
-} from '@/presentation/components';
+  IconName
+} from '@/presentation/components'
 
 const SurveyList: React.FC = () => {
   return (
@@ -26,35 +26,12 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver Resultado</footer>
           </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumDown} />
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-              <p>Qual e seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumDown} />
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-              <p>Qual e seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
+          <li></li>
         </ul>
       </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default SurveyList;
+export default SurveyList
