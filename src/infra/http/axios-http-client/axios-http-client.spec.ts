8db0857ce1c2378fd7687b282b16babcb80c76d7
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-23 10:46:25 +0900

    test: ensure AxiosHttpClient calls axios.get with correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,6 @@
 import { AxiosHttpClient } from './axios-http-client'
 import { mockAxios, mockHttpResponse } from '@/infra/test'
-import { mockPostRequest } from '@/data/test'
+import { mockPostRequest, mockGetRequest } from '@/data/test'
 import axios from 'axios'
 
 jest.mock('axios')
@@ -36,6 +36,15 @@ describe('AxiosHttpClient', () => {
       mockedAxios.post.mockRejectedValueOnce({ response: mockHttpResponse() })
       const promise = sut.post(mockPostRequest())
       expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+    })
+  })
+
+  describe('get', () => {
+    test('Shoud call axios.get with correct values', async () => {
+      const request = mockGetRequest()
+      const { sut, mockedAxios } = makekSut()
+      await sut.get(request)
+      expect(mockedAxios.get).toHaveBeenCalledWith(request.url)
     })
   })
 })
