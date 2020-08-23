5596e5445fbd634ea9aa5040ee09858f7c00e30d
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-23 13:12:40 +0900

    feat: add initial survey-list view

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -81,7 +81,7 @@ const SignUp: React.FC<Props> = ({
     }
   }
   return (
-    <div className={Styles.signup}>
+    <div className={Styles.signupWrap}>
       <Header />
       <Context.Provider value={{ state, setState }}>
         <form
