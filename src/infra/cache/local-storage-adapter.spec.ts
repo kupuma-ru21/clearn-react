26f8338e3d24801ede6d74da7cacca8dc256b228
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-25 21:25:23 +0900

    test: add getCurrentAccount to ApiContext

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -8,7 +8,7 @@ const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
 describe('LocalStorageAdapter', () => {
   beforeEach(() => localStorage.clear())
 
-  test('Should call localstorage with correct values', async () => {
+  test('Should call localstorage.setItem with correct values', async () => {
     const key = faker.database.column()
     const value = faker.random.objectElement<AccountModel>()
     makeSut().set(key, value)
@@ -16,5 +16,16 @@ describe('LocalStorageAdapter', () => {
       key,
       JSON.stringify(value)
     )
+  })
+
+  test('Should call localstorage.getItem with correct value', async () => {
+    const key = faker.database.column()
+    const value = faker.random.objectElement<AccountModel>()
+    const getItemSpy = jest
+      .spyOn(localStorage, 'getItem')
+      .mockReturnValueOnce(JSON.stringify(value))
+    const obj = makeSut().get(key)
+    expect(obj).toEqual(value)
+    expect(getItemSpy).toHaveBeenCalledWith(key)
   })
 })
