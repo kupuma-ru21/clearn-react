5f508732e4e37e402f680d485f63660e8c43b0e3
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-27 22:17:33 +0900

    refactor: move survey list item to components

---
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { IconName, Icon } from '@/presentation/components'
+import Styles from './survey-item-styles.scss'
+
+const SurveyItem: React.FC = () => {
+  return (
+    <li className={Styles.surveyItemWrap}>
+      <div className={Styles.surveyContent}>
+        <Icon className={Styles.iconWrap} iconName={IconName.thumDown} />
+        <time>
+          <span className={Styles.day}>22</span>
+          <span className={Styles.month}>03</span>
+          <span className={Styles.year}>2020</span>
+        </time>
+        <p>Qual e seu framework web favorito?</p>
+      </div>
+      <footer>Ver Resultado</footer>
+    </li>
+  )
+}
+
+export default SurveyItem
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import Styles from './survey-list-styles.scss'
-import {
-  Footer,
-  SurveyListHeader,
-  Icon,
-  IconName
-} from '@/presentation/components'
+import { Footer, SurveyListHeader } from '@/presentation/components'
 
 const SurveyList: React.FC = () => {
   return (
@@ -13,21 +8,7 @@ const SurveyList: React.FC = () => {
       <SurveyListHeader />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumDown} />
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-              <p>Qual e seu framework web favorito?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li></li>
-        </ul>
+        <ul></ul>
       </div>
       <Footer />
     </div>
