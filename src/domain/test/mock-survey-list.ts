0a16fb8b8a2a69744dc7ba23fc08f3ac54271f14
kupuma-ru21 <kupumaru21@gmail.com>
2020-08-30 13:52:43 +0900

    test: ensure SurveyItem load survey data correctolly

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -1,20 +1,24 @@
 import faker from 'faker';
 import { SurveyModel } from '@/domain/models';
 
+export const mockSurveyModel = (): SurveyModel => ({
+  id: faker.random.uuid(),
+  question: faker.random.words(10),
+  answers: [
+    {
+      image: faker.internet.url(),
+      answer: faker.random.words(4),
+    },
+    {
+      answer: faker.random.words(4),
+    },
+  ],
+  date: faker.date.recent(),
+  didAnswer: faker.random.boolean(),
+});
+
 export const mockSurveyListModel = (): SurveyModel[] => [
-  {
-    id: faker.random.uuid(),
-    question: faker.random.words(10),
-    answers: [
-      {
-        image: faker.internet.url(),
-        answer: faker.random.words(4),
-      },
-      {
-        answer: faker.random.words(4),
-      },
-    ],
-    date: faker.date.recent(),
-    didAnswer: faker.random.boolean(),
-  },
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel(),
 ];
