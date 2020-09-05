54773db54b86ef9212ed7c6b617e33c7bc46c33c
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-05 23:43:18 +0900

    test: ensure SurveyList reload on button click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -2,10 +2,9 @@ import * as Helper from '../utills/helpers';
 import * as Http from '../utills/http-mocks';
 
 const path = /surveys/;
-export const mockUnexpectedError = (): void =>
-  Http.mockServerError(path, 'GET');
-export const mockAccessDeniedError = (): void =>
-  Http.mockForbiddenError(path, 'GET');
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET');
+const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET');
+const mockSuccess = (): void => Http.mockOk(path, 'GET', 'fx:survey-list');
 
 describe('SurveyList', () => {
   beforeEach(() => {
@@ -21,6 +20,18 @@ describe('SurveyList', () => {
       'contain.text',
       'Algo de errado aconteceu. Tente novamente em breve.'
     );
+  });
+
+  it('Should reload button on click', () => {
+    mockUnexpectedError();
+    cy.visit('');
+    cy.getByTestId('error').should(
+      'contain.text',
+      'Algo de errado aconteceu. Tente novamente em breve.'
+    );
+    mockSuccess();
+    cy.getByTestId('reload').click();
+    cy.get('li:not(:empty)').should('have.length', 2);
   });
 
   it('Should logout on AccessDeniedError', () => {
@@ -41,5 +52,34 @@ describe('SurveyList', () => {
     cy.visit('');
     cy.getByTestId('logout').click();
     Helper.testUrl('/login');
+  });
+
+  it('Should present survey items', () => {
+    mockSuccess();
+    cy.visit('');
+    cy.get('li:empty').should('have.length', 4);
+    cy.get('li:not(:empty)').should('have.length', 2);
+    cy.get('li:nth-child(1)').then((li) => {
+      assert.equal(li.find('[data-testid="day"]').text(), '03');
+      assert.equal(li.find('[data-testid="month"]').text(), 'fev');
+      assert.equal(li.find('[data-testid="year"]').text(), '2018');
+      assert.equal(li.find('[data-testid="question"]').text(), 'Question 1');
+      cy.fixture('icons').then((icon) => {
+        assert.equal(li.find('[data-testid="icon"]').attr('src'), icon.thumUp);
+      });
+    });
+
+    cy.get('li:nth-child(2)').then((li) => {
+      assert.equal(li.find('[data-testid="day"]').text(), '20');
+      assert.equal(li.find('[data-testid="month"]').text(), 'out');
+      assert.equal(li.find('[data-testid="year"]').text(), '2020');
+      assert.equal(li.find('[data-testid="question"]').text(), 'Question 2');
+      cy.fixture('icons').then((icon) => {
+        assert.equal(
+          li.find('[data-testid="icon"]').attr('src'),
+          icon.thumDown
+        );
+      });
+    });
   });
 });
