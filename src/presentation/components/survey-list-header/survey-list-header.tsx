4505ce6a8027ca45469b69f1ae811c1d91b28f81
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-05 11:32:25 +0900

    feat: ensure logout calls setCurrentAccount with undefined

--- src/presentation/components/survey-list-header/survey-list-header.tsx
+++ src/presentation/components/survey-list-header/survey-list-header.tsx
@@ -1,13 +1,26 @@
-import React, { memo } from 'react';
+import React, { memo, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { ApiContext } from '@/presentation/context';
 import Styles from './survey-list-header-styles.scss';
 
 const SurveyListHeader: React.FC = () => {
+  const history = useHistory();
+  const { setCurrentAccount } = useContext(ApiContext);
+  const logout = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ): void => {
+    event.preventDefault();
+    setCurrentAccount(undefined);
+    history.replace('/login');
+  };
   return (
     <header className={Styles.headerWrap}>
       <div className={Styles.headerContent}>
         <div className={Styles.logoutWrap}>
           <span>Rodrigo</span>
-          <a href="#">Sair</a>
+          <a data-testid="logout" href="#" onClick={logout}>
+            Sair
+          </a>
         </div>
       </div>
     </header>
