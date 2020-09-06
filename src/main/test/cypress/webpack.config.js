cd3d7db48a4c146b85787759be48f26faa98c5d8
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-06 11:47:53 +0900

    chore: add webpack prod and dev configs and adjusment cypress webpack

--- src/main/test/cypress/webpack.config.js
+++ src/main/test/cypress/webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+      {
         test: /\.ts$/,
         exclude: [/node_modules/],
         use: [
