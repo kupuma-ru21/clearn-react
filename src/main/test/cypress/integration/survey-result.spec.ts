5629a095bbc308dcd0f70cc8619d358bf3eb78d5
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-12 14:03:46 +0900

    test: ensure SurveyResult reloads on reload button on click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -3,6 +3,7 @@ import * as Http from '../utills/http-mocks';
 
 const path = /surveys/;
 const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET');
+const mockSuccess = (): void => Http.mockOk(path, 'GET', 'fx:survey-result');
 
 describe('SurveyList', () => {
   beforeEach(() => {
@@ -18,5 +19,17 @@ describe('SurveyList', () => {
       'contain.text',
       'Algo de errado aconteceu. Tente novamente em breve.'
     );
+  });
+
+  it('Should reload button on click', () => {
+    mockUnexpectedError();
+    cy.visit('/surveys/any_id');
+    cy.getByTestId('error').should(
+      'contain.text',
+      'Algo de errado aconteceu. Tente novamente em breve.'
+    );
+    mockSuccess();
+    cy.getByTestId('reload').click();
+    cy.getByTestId('question').should('exist');
   });
 });
